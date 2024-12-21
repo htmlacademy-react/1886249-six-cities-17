422c37fa2bee9225f3208227e088233f04650c7f
anna <anka-kudrow@mail.ru>
2024-12-21 20:26:07 +0200

    add useEffect for hiding sort menu

--- src/components/sorting-cities/sorting-cities.tsx
+++ src/components/sorting-cities/sorting-cities.tsx
@@ -1,22 +1,39 @@
 import { useAppDispatch, useAppSelector } from '@/hooks';
 import { SortItem } from '@/libs/const';
 import { changeCurrentSort } from '@/store/action';
+import { useEffect, useRef, useState } from 'react';
 
 function SortingCities() {
   const currentSort = useAppSelector((state) => state.currentSort);
   const dispatch = useAppDispatch();
+  const sortSpanRef = useRef<HTMLElement>(null);
+  const [isSortMenuOpened, setSortMenuOpened] = useState(false);
+  useEffect(() => {
+    const hideSortMenu = (evt: MouseEvent) => {
+      if (evt.target instanceof HTMLElement && sortSpanRef.current && !sortSpanRef.current.contains(evt.target)) {
+        setSortMenuOpened(false);
+      }
+    };
+
+    document.addEventListener('click', hideSortMenu);
+    return () => {
+      document.removeEventListener('click', hideSortMenu);
+    };
+  }, []);
+
   return (
     <form className="places__sorting" action="#" method="get">
       <span className="places__sorting-caption">Sort by</span>
-      <span className="places__sorting-type" tabIndex={0}>
+      <span className="places__sorting-type" tabIndex={0} ref={sortSpanRef} onClick={()=> setSortMenuOpened(((lastOpened) => !lastOpened))}>
         {currentSort}
         <svg className="places__sorting-arrow" width={7} height={4}>
           <use xlinkHref="#icon-arrow-select" />
         </svg>
       </span>
-      <ul className="places__options places__options--custom places__options--opened">
-        {Object.values(SortItem).map((sortIem) => <li key={sortIem} className={currentSort === sortIem ? 'places__option places__option--active' : 'places__option'} tabIndex={0} onClick={()=>(dispatch(changeCurrentSort(sortIem)))}>{sortIem}</li>)}
-      </ul>
+      {isSortMenuOpened ?
+        <ul className="places__options places__options--custom places__options--opened">
+          {Object.values(SortItem).map((sortIem) => <li key={sortIem} className={currentSort === sortIem ? 'places__option places__option--active' : 'places__option'} tabIndex={0} onClick={()=>(dispatch(changeCurrentSort(sortIem)))}>{sortIem}</li>)}
+        </ul> : null}
     </form>
   );
 }
