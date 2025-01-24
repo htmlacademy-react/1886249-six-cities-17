4f0717897267b65bae81c038e3134b6b84350d1e
anna <anka-kudrow@mail.ru>
2025-01-24 18:32:31 +0400

    add sorting of favourits cards according cities

--- src/pages/favourites-page/favourites-page.tsx
+++ src/pages/favourites-page/favourites-page.tsx
@@ -1,4 +1,4 @@
-import { CardType } from '../../libs/const';
+import { CardType, Cities } from '../../libs/const';
 import Footer from '../../components/footer/footer';
 import OfferCard from '../../components/main/offer-card-main/offer-card';
 import { FavouritesEmpty } from '../favourites-empty/favorites-empty';
@@ -10,28 +10,45 @@ import { favouritesSelectors } from '@/storage/slices/favorites';
 export default function FavouritesPage() {
 
   const favourites = useAppSelector(favouritesSelectors.selectFavourites);
+  const groupedFavourites = favourites.reduce<Record<string, OfferCardPrew[]>>((acc, card) => {
+    if (!acc[card.city.name]) {
+      acc[card.city.name] = [];
+    }
+    acc[card.city.name].push(card);
+    return acc;
+  }, {});
 
   return (
     <>
-      {favourites.length
+      {favourites.length > 0
         ?
         <main className="page__main page__main--favorites">
           <div className="page__favorites-container container">
             <section className="favorites">
               <h1 className="favorites__title">Saved listing</h1>
               <ul className="favorites__list">
-                <li className="favorites__locations-items">
-                  <div className="favorites__locations locations locations--current">
-                    <div className="locations__item">
-                      <a className="locations__item-link" href="#">
-                        <span>Amsterdam</span>
-                      </a>
-                    </div>
-                  </div>
-                  <div className="favorites__places">
-                    {favourites.map((favOffer: OfferCardPrew) => <OfferCard offer={favOffer} key={favOffer.id} offerCardType={CardType.FavoritesCard}/>)}
-                  </div>
-                </li>
+                {Object.values(Cities).map((city) => (
+                  groupedFavourites[city] && (
+                    <li key={city} className="favorites__locations-items">
+                      <div className="favorites__locations locations locations--current">
+                        <div className="locations__item">
+                          <a className="locations__item-link" href="#">
+                            <span>{city}</span>
+                          </a>
+                        </div>
+                      </div>
+                      <div className="favorites__places">
+                        {groupedFavourites[city].map((offer) => (
+                          <OfferCard
+                            offer={offer}
+                            key={offer.id}
+                            offerCardType={CardType.FavoritesCard}
+                          />
+                        ))}
+                      </div>
+                    </li>
+                  )
+                ))}
               </ul>
             </section>
           </div>
